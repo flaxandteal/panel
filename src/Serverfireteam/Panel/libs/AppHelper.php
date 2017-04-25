21cea12588b397c9eb7454456b612d1353d6ebc0
Phil Weir <phil.weir@flaxandteal.co.uk>
2017-04-25 16:03:18 +0100

    update to DetectsApplicationNamespace

--- src/Serverfireteam/Panel/libs/AppHelper.php
+++ src/Serverfireteam/Panel/libs/AppHelper.php
@@ -1,43 +1,48 @@
 <?php
 namespace Serverfireteam\Panel\libs;
 
+use Illuminate\Console\DetectsApplicationNamespace;
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class AppHelper {
-    use \Illuminate\Console\AppNamespaceDetectorTrait;
+class AppHelper
+{
+    use DetectsApplicationNamespace;
 
-    public function getNameSpace(){
+    public function getNameSpace()
+    {
         return $this->getAppNamespace();
     }
 
-    public static function validName($name) {
-	   return strpos($name, '.') !== 0;
+    public static function validName($name)
+    {
+        return strpos($name, '.') !== 0;
     }
 
-    public static function access($attr, $path, $data, $volume) {
-    	if (strpos(basename($path), '.') === 0) {
-    	        return !($attr == 'read');
-    	} else {
-    	        return null;
-    	}
+    public static function access($attr, $path, $data, $volume)
+    {
+        if (strpos(basename($path), '.') === 0) {
+                return !($attr == 'read');
+        } else {
+                return null;
+        }
     }
 
-    public function getModel($entity) {
+    public function getModel($entity)
+    {
         $entity = str_replace(':', '\\', $entity);
-        if ( in_array($entity, \Serverfireteam\Panel\Link::getMainUrls()) ) {
+        if (in_array($entity, \Serverfireteam\Panel\Link::getMainUrls())) {
             $modelClass = 'Serverfireteam\\Panel\\'.$entity;
         } else {
             if (!empty(\Config::get('panel.modelPath'))) {
                 $modelClass = $this->getNameSpace() . \Config::get('panel.modelPath') . '\\' . $entity;
-            }
-            else {
+            } else {
                 $modelClass = $this->getNameSpace() . $entity;
             }
-            
         }
         return $modelClass;
     }
